e10db4ec6ce0322b946e4e7e258a72ffddf78f38
David Heinemeier Hansson <david@hey.com>
2026-02-15 11:48:35 +0100

    Fix scroll direction

--- install/dock.sh
+++ install/dock.sh
@@ -6,5 +6,8 @@ defaults write com.apple.dock orientation -string "right"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock tilesize -int 43
 
+# Normal (non-natural) scroll direction
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
 # Restart dock to apply changes
 killall Dock
