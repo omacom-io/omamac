5b0751867baf4b44f070e49196fe911097aa1c60
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-02 12:29:19 -0500

    Clear Dock apps and folders on install

--- install/mac.sh
+++ install/mac.sh
@@ -6,6 +6,10 @@ defaults write com.apple.dock orientation -string "right"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock tilesize -int 43
 
+# Clear all apps and folders from the Dock
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-others -array
+
 # Normal (non-natural) scroll direction
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
